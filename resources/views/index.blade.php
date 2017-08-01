368a8034846191b18984dc445ba7a5b3a62efc3f
rokite <rokite@gmail.com>
2017-08-01 09:28:03 +0800

    修改备案号

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,7 +42,7 @@
                 <div class="info">
                     Copyright&nbsp;&copy;&nbsp;2017&nbsp;,fangcuntang&nbsp;Co.,Ltd.All&nbsp;Rights&nbsp;Reserved.<br>
                     宜兴方寸堂文化传媒有限公司&ensp;|&ensp;<a href="" class="law">法律声明</a><br>
-                    <span class="bottom">沪ICP备12022967号-1&nbsp;沪公网安备11010502025474</span>
+                    <span class="bottom">苏公网安备32028202000436号 苏ICP备14043090号-4</span>
                 </div>
             </div>
         </div>
