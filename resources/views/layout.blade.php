16131157ed70f864172de56cd1b4b20481beba1c
rokite <rokite@gmail.com>
2017-08-29 10:03:51 +0800

    add help

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta charset="utf-8">
     <title>{{ $title or "404" }}</title>
     <meta name="keywords" content="">
     <meta name="description" content="">
