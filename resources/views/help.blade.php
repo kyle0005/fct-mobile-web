aef947c592b28e43cc36f1f66b0461437030b084
rokite <rokite@gmail.com>
2017-08-30 13:40:18 +0800

    modify head

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -68,11 +68,7 @@
     </template>
     <template id="articledetail">
         <section class="service">
-            <div class="service-container">
-                <section class="sec">
-                    <div class="title">@{{ article.title }}</div>
-                    <div class="content" v-html="article.content"></div>
-                </section>
+            <div class="service-container" v-html="article.content">
             </div>
         </section>
     </template>
