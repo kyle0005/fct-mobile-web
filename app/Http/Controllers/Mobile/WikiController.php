b5009f1e4b81c5053fcc5353cc95cec7a159b3fe
rokite <rokite@gmail.com>
2017-09-04 11:08:50 +0800

    wiki share logo

--- app/Http/Controllers/Mobile/WikiController.php
+++ app/Http/Controllers/Mobile/WikiController.php
@@ -44,7 +44,7 @@ class WikiController extends BaseController
             'share' => [
                 'title' => fct_title('百科'),
                 'link' => $shareUrl,
-                'img' => fct_cdn('/images/fc_logo.png'),
+                'img' => fct_cdn('/images/wiki-logo.png'),
                 'desc' => '方寸堂百科，专注于紫砂领域知识的创建与分享。',
             ]
         ]);
