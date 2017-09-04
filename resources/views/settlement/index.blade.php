f6faf5117f7243c98628f061be6598567eb8db16
rokite <rokite@gmail.com>
2017-09-04 14:45:00 +0800

    总计带两位小数

--- resources/views/settlement/index.blade.php
+++ resources/views/settlement/index.blade.php
@@ -28,7 +28,7 @@
                     </li>
                 </ul>
                 <div class="total">
-                    <div class="inner">共<span class="pri">@{{ item.totalCount }}</span>件宝贝&nbsp;合计佣金：<span class="pri"><small class="pri-mark">￥</small>@{{ toFloat(item.commission) }}</span></div>
+                    <div class="inner">共<span class="pri">@{{ item.totalCount }}</span>件宝贝&nbsp;合计佣金：<span class="pri"><small class="pri-mark">￥</small>@{{ item.commission }}</span></div>
                 </div>
             </div>
         </div>
