bac43b4c17ff0067438e6a488411b2e1994bdc98
rokite <rokite@gmail.com>
2017-09-05 10:43:28 +0800

    pc首页修改

--- app/helpers.php
+++ app/helpers.php
@@ -45,3 +45,37 @@ if (!function_exists('fct_title'))
         return ($title ? $title . ' - ' : '') . '方寸堂';
     }
 }
+
+if (!function_exists('is_mobile'))
+{
+    function is_mobile()
+    {
+        $agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+        $is_pc = (strpos($agent, 'windows nt')) ? true : false;
+        $is_mac = (strpos($agent, 'mac os')) ? true : false;
+        $is_iphone = (strpos($agent, 'iphone')) ? true : false;
+        $is_android = (strpos($agent, 'android')) ? true : false;
+        $is_ipad = (strpos($agent, 'ipad')) ? true : false;
+
+
+        if($is_pc){
+            return  false;
+        }
+
+        if($is_mac){
+            return  true;
+        }
+
+        if($is_iphone){
+            return  true;
+        }
+
+        if($is_android){
+            return  true;
+        }
+
+        if($is_ipad){
+            return  true;
+        }
+    }
+}
