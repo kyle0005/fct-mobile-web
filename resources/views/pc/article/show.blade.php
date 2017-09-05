c22c5b5a290381552b882974615052c7b97263a2
rokite <rokite@gmail.com>
2017-09-05 11:06:20 +0800

    pc首页修改

--- resources/views/pc/article/show.blade.php
+++ resources/views/pc/article/show.blade.php
@@ -24,25 +24,8 @@
                 {!! $html !!}
             </div>
         </div>
-        <a href="javascript:;" class="back" id="back">
+        <a href="/" class="back" id="back">
             <img src="public/img/fct/p_back.png"><span class="txt">返回全部</span>
         </a>
-    </div>
-</div>
-
-<div id="ywPage" class="yw-page">
-    <div id="contact" class="yw-footer">
-        <div class="yw-constr">
-            <div class="yw-footerbox">
-                <p class="yw-footer-copyright">
-                    Copyright&nbsp&copy;&nbsp;<script>document.write((new Date).getFullYear())</script>&nbsp;,宜兴方寸堂文化传媒有限公司<span class="br">版权所有</span></p>
-                <p class="yw-footer-copyrightmore">苏公安备32028202000436号&nbsp;苏ICP备14043090号-4</p>
-                <p class="yw-footer-copyrightmore">联系电话：<a href="tel:4000510570" class="law">400-0510-570</a></p></div>
-            <div class="yw-footer-share">
-                <a href="{{ fct_cdn('/img/fct/qrcode-gzh.jpg') }}"
-                   class="icon-share icon-share-weixin" title="方寸堂官方微信公众号" target="_blank">方寸堂官方微信公众号</a>
-                <a href="javascript:;" class="icon-share icon-share-weibo" title="方寸堂官方微博" target="_blank">方寸堂官方微博</a>
-            </div>
-        </div>
     </div>
 </div>
