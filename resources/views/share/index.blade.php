023e9d40f9f1e0867ab3a41b18eefc78ea806a04
rokite <rokite@gmail.com>
2017-09-06 15:13:03 +0800

    fix refund save image

--- resources/views/share/index.blade.php
+++ resources/views/share/index.blade.php
@@ -48,7 +48,7 @@
                       <span class="title">@{{ item.name }}</span>
                     <span class="t1" v-if="item.price instanceof Array">价格：<small class="pri-mark">￥</small>@{{ item.price[0] }}&sim;<small class="pri-mark">￥</small>@{{ item.price[1] }}</span>
                       <span class="t1" v-else>价格：<small class="pri-mark">￥</small>@{{ item.price }}</span>
-                      <span class="t2" v-if="item.commission instanceof Array">利润：<strong class="pri"><small class="pri-mark">￥</small>@{{ item.commission[0] }}&sim;<small class="pri-mark">￥</small>@{{ item.commission[1] }}</strong></span>
+                      <span class="t2" v-if="item.commission instanceof Array">佣金：<strong class="pri"><small class="pri-mark">￥</small>@{{ item.commission[0] }}&sim;<small class="pri-mark">￥</small>@{{ item.commission[1] }}</strong></span>
                       <span class="t2" v-else>利润：<strong class="pri"><small class="pri-mark">￥</small>@{{ item.commission }}</strong></span>
                     </span>
                     <span class="right"><img src="{{ fct_cdn('/images/share.png') }}"></span>
