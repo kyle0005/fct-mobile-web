ea7598b04df0555e1ef1e430af46e4793461b6c4
rokite <rokite@gmail.com>
2017-10-09 18:43:25 +0800

    pc index bug

--- app/Http/Controllers/Mobile/MemberAddressController.php
+++ app/Http/Controllers/Mobile/MemberAddressController.php
@@ -47,6 +47,7 @@ class MemberAddressController extends BaseController
         try
         {
             $result = MemberAddress::getAddress($id);
+            $this->cacheRedirectSourceUrl('', true);
         }
         catch (BusinessException $e)
         {
