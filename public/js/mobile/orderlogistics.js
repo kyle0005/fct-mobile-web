fdb3f73e736eb2868b830d160f4ad2bf9ddb970b
rokite <rokite@gmail.com>
2017-10-11 15:20:14 +0800

    change js folder

---
+++ public/js/mobile/orderlogistics.js
@@ -0,0 +1,1 @@
+"use strict";var app=new Vue({computed:{},mounted:function(){},data:{logistics:config.logistics},watch:{},methods:{}}).$mount("#logistics");
---
+++ public/js/mobile/pay.js
@@ -0,0 +1,1 @@
+"use strict";var app=new Vue({computed:{},mounted:function(){},activated:function(){},deactivated:function(){},data:{pay_list:[{title:"alipay",img:"public"},{},{}]},watch:{},methods:{},components:{}}).$mount("#pay");
