c72d9bd1e92178ebc0fad55f3ef928059b51d85d
rokite <rokite@gmail.com>
2017-10-12 09:46:24 +0800

    qq logs http to https

--- resources/views/wiki/index.blade.php
+++ resources/views/wiki/index.blade.php
@@ -123,7 +123,7 @@
         var _mtac = {};
         (function() {
             var mta = document.createElement("script");
-            mta.src = "http://pingjs.qq.com/h5/stats.js?v2.0.2";
+            mta.src = "https://pingjs.qq.com/h5/stats.js?v2.0.2";
             mta.setAttribute("name", "MTAH5");
             mta.setAttribute("sid", "500500357");
             var s = document.getElementsByTagName("script")[0];
