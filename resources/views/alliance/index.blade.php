d4bff2ea752f704915ef558656c95417316c7da1
rokite <rokite@gmail.com>
2018-04-28 18:05:57 +0800

    联盟和专题

--- resources/views/alliance/index.blade.php
+++ resources/views/alliance/index.blade.php
@@ -42,8 +42,8 @@
         <section class="invite-container">
             <div class="title">
                 <div class="t">邀请开店</div>
-                <div class="vt">有<span class="n">@{{ union.inviteCount }}</span>张邀请码</div>
-                <a href="javascript:;" class="btn" @click="invite"><img src="{{ fct_cdn('/img/mobile/touch.png') }}"></a>
+                <div class="vt" v-if="union.inviteCount > 0">有<span class="n">@{{ union.inviteCount }}</span>张邀请码</div>
+                <a href="javascript:;" class="btn" @click="invite"><img src="{{ fct_cdn('/img/mobile/touch.png') }}" v-if="union.inviteCount > 0"></a>
             </div>
             <ul class="detail">
                 <li class="items">
