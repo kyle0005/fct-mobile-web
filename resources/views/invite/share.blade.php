0fdae15b5ca8fef851d03e20cfbbb874f86343f9
rokite <rokite@gmail.com>
2018-05-14 13:28:30 +0800

    add invite

--- resources/views/invite/share.blade.php
+++ resources/views/invite/share.blade.php
@@ -18,8 +18,8 @@
         config.backgroundUrl = {!! json_encode($backgroundUrl, JSON_UNESCAPED_UNICODE) !!};
         config.logoUrl = {!! json_encode($logoUrl, JSON_UNESCAPED_UNICODE) !!};
         config.textObj = {
-                "textInfo": "，领取了方寸堂200元，特来邀请您一起拿红包",
-                "textQrcode": "长按识别二维码获取奖金",
+                "textInfo": "，领取了方寸堂200元，特来邀请您一起拿紫砂壶",
+                "textQrcode": "长按识别二维码获取红包",
                 "textTip": "活动提示",
                 "textLine1": "仅限于使用手机号注册并通过微信授权的用户有效",
                 "textLine2": "严禁恶意刷金，一经发现将取消相关奖励",
