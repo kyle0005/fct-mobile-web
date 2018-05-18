761bdd1a2fc8c859fb7649b5923d0f3de7281e8b
rokite <rokite@gmail.com>
2018-05-18 10:30:23 +0800

    删除艺人作品数

--- resources/views/artist/index.blade.php
+++ resources/views/artist/index.blade.php
@@ -11,7 +11,6 @@
                     <span class="art-con">
                         <span class="title">@{{ item.name }}</span><span class="">@{{ item.title }}</span><br>
                         <span class="product-num overText"><i class="fa fa-heart-o"></i>@{{ item.followCount }}</span>
-                        <span class="product-num overText"><img src="{{ fct_cdn('/img/mobile/zhh.png') }}" class="i-img">@{{ item.goodsCount }}</span>
                         <span class="intro overTextH3">@{{ item.intro }}</span>
                     </span>
                 </a>
