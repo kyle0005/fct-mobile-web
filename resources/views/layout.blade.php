a041913541ecf4ef09e46e6981f71d914af9f64d
rokite <rokite@gmail.com>
2018-05-24 09:28:07 +0800

    javascript调试工具

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -81,5 +81,7 @@
         }
     </script>
     @yield('javascript')
+    <script src="https://cdn.bootcss.com/eruda/1.4.3/eruda.min.js"></script>
+    <script>eruda.init();</script>
 </body>
 </html>
