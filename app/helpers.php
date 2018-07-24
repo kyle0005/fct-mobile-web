a4048a3002714d82771f3928fecebfc3ad16993e
rokite <rokite@gmail.com>
2018-07-24 15:22:12 +0800

    删除分享图的subTitle

--- app/helpers.php
+++ app/helpers.php
@@ -18,7 +18,7 @@ if (!function_exists('fct_cdn'))
             $domain = env('APP_SECURE') ? 'https:' : 'http:';
         }
         $domain .= env('STATIC_URL', '');
-        return rtrim($domain, '/') . $path . ($hasEnd?'?_rd=201807201515': '');
+        return rtrim($domain, '/') . $path . ($hasEnd?'?_rd=201807241521': '');
     }
 }
 
