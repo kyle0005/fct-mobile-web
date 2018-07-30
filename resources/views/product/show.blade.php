586fa0ea61dc5b88a2547e2d18ac92647399fd6b
rokite <rokite@gmail.com>
2018-07-30 10:41:39 +0800

    宝贝详情添加图片查看与匿名bug修复

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -159,7 +159,7 @@
                 <mVideo v-if="product.video" :poster="product.video.poster" :url="product.video.url" id="videotop"></mVideo>
                 <m-swipe v-else swipeid="swipe" ref="swiper" :autoplay="0" effect="slide">
                     <div v-for="(top, index) in tops" class="swiper-slide" slot="swiper-con">
-                        <a :href="top.url" class="link">
+                        <a href="javascript:;" class="link" @click="preview(top)">
                             <img :data-src="top" class="swiper-lazy silde-img">
                         </a>
                     </div>
@@ -249,10 +249,10 @@
                     <ul class="top-list">
                         <li v-for="(item, index) in artist" :class="{red:index===art_num}">
                             <a href="javascript:;" @click="loadsingle(index)">
-                                <span class="img-container">
-                                  <img :src="item.headPortrait">
-                                </span>
-                                <span class="name-container overText">@{{ item.name }}</span>
+                                <span class="img-container" v-if="item.isAnonymous != 1"><img :src="item.headPortrait"/></span>
+                                <span class="img-container" v-else><img src="{{ fct_cdn('/img/head.jpg') }}"/></span>
+                                <span class="name-container overText" v-if="item.isAnonymous != 1">@{{ item.name }}</span>
+                                <span class="name-container overText" v-else>匿名</span>
                             </a>
                         </li>
                     </ul>
